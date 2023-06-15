32374342f74fe97436c71c035f30ba168bd77c5f
chathura456 <chathuracde@gmail.com>
2023-06-16 00:14:48 +0530

    not show images fixed

--- TutorHQ/Views/Student-Data/Add-Student.Designer.cs
+++ TutorHQ/Views/Student-Data/Add-Student.Designer.cs
@@ -39,7 +39,6 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel25 = new System.Windows.Forms.Panel();
             this.label9 = new System.Windows.Forms.Label();
@@ -101,6 +100,7 @@
             this.panel47 = new System.Windows.Forms.Panel();
             this.button10 = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button7 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
@@ -161,6 +161,7 @@
             this.button6.FlatAppearance.BorderSize = 0;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button6.Image = global::TutorHQ.Properties.Resources.icons8_logout_24__1x_;
             this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button6.Location = new System.Drawing.Point(0, 495);
             this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -179,6 +180,7 @@
             this.button5.FlatAppearance.BorderSize = 0;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button5.Image = global::TutorHQ.Properties.Resources.icons8_training_30;
             this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button5.Location = new System.Drawing.Point(0, 293);
             this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -197,6 +199,7 @@
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button4.Image = global::TutorHQ.Properties.Resources.icons8_student_30;
             this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button4.Location = new System.Drawing.Point(0, 238);
             this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -215,6 +218,7 @@
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button3.Image = global::TutorHQ.Properties.Resources.icons8_schedule_26;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button3.Location = new System.Drawing.Point(0, 183);
             this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -233,6 +237,7 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button2.Image = global::TutorHQ.Properties.Resources.icons8_payment_30;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button2.Location = new System.Drawing.Point(0, 128);
             this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -251,6 +256,7 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button1.Image = global::TutorHQ.Properties.Resources.icons8_checked_user_male_26;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.Location = new System.Drawing.Point(0, 73);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -298,20 +304,6 @@
             this.label2.Size = new System.Drawing.Size(243, 57);
             this.label2.TabIndex = 12;
             this.label2.Text = "Add Student";
-            // 
-            // button7
-            // 
-            this.button7.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button7.FlatAppearance.BorderSize = 0;
-            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Image = global::TutorHQ.Properties.Resources.icons8_go_back_24;
-            this.button7.Location = new System.Drawing.Point(735, 0);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(63, 73);
-            this.button7.TabIndex = 13;
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // panel4
             // 
@@ -953,6 +945,20 @@
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Image = global::TutorHQ.Properties.Resources.icons8_go_back_24;
+            this.button7.Location = new System.Drawing.Point(735, 0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 73);
+            this.button7.TabIndex = 13;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Add_Student
             // 
